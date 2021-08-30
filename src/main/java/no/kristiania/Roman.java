b74418c3a073161dfa2127851822215cd549e47e
q10357 <isch004@student.kristiania.no>
2021-08-30 20:30:43 +0200

    Initial commit

--- src/main/java/no/kristiania/Roman.java
+++ src/main/java/no/kristiania/Roman.java
@@ -2,6 +2,6 @@ package no.kristiania;
 
 public class Roman {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello worl");
     }
 }
