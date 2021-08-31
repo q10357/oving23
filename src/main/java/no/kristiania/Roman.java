003dcf01aa32c2c61e4c37f82e8fa029e4c764f1
q10357 <isch004@student.kristiania.no>
2021-08-31 14:12:02 +0200

    Made new test

--- src/main/java/no/kristiania/Roman.java
+++ src/main/java/no/kristiania/Roman.java
@@ -12,7 +12,39 @@ public class Roman {
         System.out.printf("%d in roman notation is %s", numberInput, romanNumber);
     }
 
-
-
-
+    public static String toRoman(int number) {
+        String romanNumber = "";
+        while(number >= 3) {
+            if (number == 4) {
+                number = 0;
+                romanNumber += "IV";
+            }else if (number == 9) {
+                number = 0;
+                romanNumber += "IX";
+            }else if (number >= 1000){
+                number = number - 1000;
+                romanNumber += "M";
+            }else if (number >= 500){
+                number = number - 500;
+                romanNumber += "D";
+            }else if (number >= 100){
+                number = number - 100;
+                romanNumber += "C";
+            }else if (number >= 50){
+                number = number - 50;
+                romanNumber += "L";
+            }else if (number >= 10) {
+                number = number - 10;
+                romanNumber += "X";
+            }else if (number >= 5 && number < 10) {
+                number = number - 5;
+                romanNumber += "V";
+            }
+        }
+        for (int i = 0; i < number; i++) {
+            romanNumber += "I";
+        }
+        System.out.println(romanNumber);
+        return romanNumber;
+    }
 }
--- src/test/java/no/Kristiania/RomanTest.java
+++ src/test/java/no/Kristiania/RomanTest.java
@@ -1,5 +1,6 @@
 package no.Kristiania;
 
+import no.kristiania.Roman;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,43 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanTest {
 
     @Test
-    void shuldTranslateOneToI(){
-        assertEquals("I", toRoman(1));
+    void shouldTranslateOneToI(int number){
+        assertEquals("I", Roman.toRoman(1));
     }
 
-    private static String toRoman(int number) {
-        String romanNumber = "";
-        while(number >= 3) {
-            if (number == 4) {
-                number = 0;
-                romanNumber += "IV";
-            }else if (number == 9) {
-                number = 0;
-                romanNumber += "IX";
-            }else if (number >= 1000){
-                number = number - 1000;
-                romanNumber += "M";
-            }else if (number >= 500){
-                number = number - 500;
-                romanNumber += "D";
-            }else if (number >= 100){
-                number = number - 100;
-                romanNumber += "C";
-            }else if (number >= 50){
-                number = number - 50;
-                romanNumber += "L";
-            }else if (number >= 10) {
-                number = number - 10;
-                romanNumber += "X";
-            }else if (number >= 5 && number < 10) {
-                number = number - 5;
-                romanNumber += "V";
-            }
-        }
-        for (int i = 0; i < number; i++) {
-            romanNumber += "I";
-        }
-        System.out.println(romanNumber);
-        return romanNumber;
-    }
 }
